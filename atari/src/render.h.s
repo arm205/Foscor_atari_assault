0898c3ae0eb0695273636c96e8433e2bea37375c
MiguelTeruel <mtc41@gcloud.ua.es>
2021-10-04 17:48:34 +0200

    jerarquizado de render

--- atari/src/render.h.s
+++ atari/src/render.h.s
@@ -1,2 +1,8 @@
 .globl _render_Entity
 .globl _render_sys_update 
+.globl _render_INIT
+
+;; instrucciones utiles
+.globl cpct_disableFirmware_asm
+.globl cpct_drawSolidBox_asm
+.globl cpct_getScreenPtr_asm
--- atari/src/render.s
+++ atari/src/render.s
@@ -2,15 +2,9 @@
 ;; RENDER.S -- Dibuja por pantalla.
 ;;
 
-;; instrucciones utiles
-.globl cpct_disableFirmware_asm
-.globl cpct_drawSolidBox_asm
-.globl cpct_getScreenPtr_asm
-
 ;; RENDER AN ENTITY
 ;;      INPUT: IX
-
-_render_Entity::
+_render_Entity:: ;;importante: actualizar con la posibilidad de abrir sprites.
 
     ld de, #0xC000
     ld b, 2(ix) ;;pos_y
@@ -26,5 +20,34 @@ _render_Entity::
 
     ret
 
-_render_sys_update ::
+;; ERASE AN ENTITY
+;;      INPUT: IX
+
+_erase_Entity::
+
+    ld de, #0xC000
+    ld b, 2(ix) ;;pos_y
+    ld c, 1(ix) ;;pos_x
+    call cpct_getScreenPtr_asm ;;entidad que comenzara a dibujarse en la pos (x,y)
+
+    ex de, hl
+
+    ld a, #0x00 ;;color
+    ld c, 3(ix) ;;ancho
+    ld b, 4(ix) ;;alto
+    call cpct_drawSolidBox_asm ;;dibuja un cuadrado con esas dimensiones.
+
+    ret
+
+;; RENDER INIT (llamado desde GAME)
+_render_sys_init::
+    call _render_Entity
+ret
+
+_render_sys_update::
+
+    call _render_Entity
+ret
+
+_render_sys_terminate::
 ret
