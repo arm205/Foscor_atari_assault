929b27c45290537952d6cac99a3241f9faaad2e2
arm205 <arm205@gcloud.ua.es>
2021-10-06 14:36:10 +0200

    Poniendo cajas otra vez (?)

--- atari/src/game.s
+++ atari/src/game.s
@@ -9,35 +9,23 @@
 
 
 
-;                   _name, _x, _y, _w, _h, _vx, _vy, _c, _b, _dest_c
-
-DefineEnemyEntity enemy, 20, 20, 4, 8, -1, 0, 0xFF, 1
-
-DefineEnemy2Entity enemy2, 20, 40, 4, 8, -1, 0, 0xFF, 2
+DefineEnemyEntity enemy, 20, 80, 4, 8, -1, 0, 0xFF, 1
 
 DefinePlayerEntity player, 20, 180, 2, 8, -1, 0, 0x0F, 0
 
-DefineCajaEntity caja, 40, 100, 2, 8, 0, 0, 0xF0, 0
-
-;DefineBalaEntity bala, 20, 180, 2, 8, 0, 0, 0xF0, 0, 2
 final_text: .asciz "GAME OVER"
 
 
 man_game_init::
     call E_M_init
 
-    call rendersys_init
+    call _render_sys_init
     call physics_sys_init
     call input_init
 
     ld hl, #enemy
     call man_game_entity_creator
-    ld hl, #enemy2
-    call man_game_entity_creator
-    ld hl, #caja
-;    call man_game_entity_creator
-;    ld hl, #bala
-    call man_game_entity_creator
+
     ld hl, #player
     call man_game_entity_creator
 
@@ -50,30 +38,21 @@ ret
 man_game_entity_creator::
     call E_M_create
 ret
-
-
-
-
-
-
 man_game_update::
  
    ;; Init system
     call E_M_getEntityArray
-   call input_update
+    call input_update
     call E_M_getEntityArray
-   call ia_update
-   call E_M_getEntityArray
-   call collider_update
+    call ia_update
     call E_M_getEntityArray
-   call physics_sys_update
+    call collider_update
+    call E_M_getEntityArray
+    call physics_sys_update
    ret
-
-
-
 man_game_render::
     call E_M_getEntityArray
-    call rendersys_update
+    call _render_ents_update
 ret
 
 man_game_end::
