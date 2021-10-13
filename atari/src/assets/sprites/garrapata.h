32daf756e1231d75ab86d4a38df80234645ab755
Sibada98 <jsl63@alu.ua.es>
2021-10-13 16:19:20 +0200

    *assets

---
+++ atari/src/assets/sprites/garrapata.h
@@ -0,0 +1,33 @@
+// Data created with Img2CPC - (c) Retroworks - 2007-2017
+#ifndef _IMG_GARRAPATA_H_
+#define _IMG_GARRAPATA_H_
+
+#include <types.h>
+extern const u8 pal_main[16];
+
+#define G_ARRAY_0_W 8
+#define G_ARRAY_0_H 32
+extern const u8 g_array_0[8 * 32];
+#define G_ARRAY_1_W 8
+#define G_ARRAY_1_H 32
+extern const u8 g_array_1[8 * 32];
+#define G_ARRAY_2_W 8
+#define G_ARRAY_2_H 32
+extern const u8 g_array_2[8 * 32];
+#define G_ARRAY_3_W 8
+#define G_ARRAY_3_H 32
+extern const u8 g_array_3[8 * 32];
+#define G_ARRAY_4_W 8
+#define G_ARRAY_4_H 32
+extern const u8 g_array_4[8 * 32];
+#define G_ARRAY_5_W 8
+#define G_ARRAY_5_H 32
+extern const u8 g_array_5[8 * 32];
+#define G_ARRAY_6_W 8
+#define G_ARRAY_6_H 32
+extern const u8 g_array_6[8 * 32];
+#define G_ARRAY_7_W 8
+#define G_ARRAY_7_H 32
+extern const u8 g_array_7[8 * 32];
+
+#endif
---
+++ atari/src/assets/tiles/tileset_prueba.h.s
@@ -0,0 +1,18 @@
+;;
+;; File img/tilemap/tileset_prueba.tmx converted to csv using cpct_tmx2data [Wed Oct 13 16:17:03 2021]
+;;   * Visible Layers:  1
+;;   * Layer Width:     20
+;;   * Layer Height:    25
+;;   * Bits per tile:   8
+;;   * Layer Bytes:     500 (20 x 25 items, 8 bits per item)
+;;   * Total Bytes:     500 (500 x 1, bytes per layer times layers)
+;;
+;;#### Width and height constants ####
+_tilemap_W = 20
+_tilemap_H = 25
+
+;;#### Converted layer tilemaps ####
+;;   Visible layers: 1
+;;
+.globl _tilemap
+_tilemap_SIZE = 500
