3cb717edb323b6a68ad4286fb2cb62bea3e4d5f9
arm205 <arm205@gcloud.ua.es>
2021-10-30 18:07:27 +0200

    mover player mapa 6

--- atari/src/level_manager.h.s
+++ atari/src/level_manager.h.s
@@ -119,7 +119,7 @@ _level_5::
 
 _level_6::
     .dw     #_level_06_pack_end         ;;TileMap
-    .db     38,0                       ;;Posicion Player
+    .db     36,0                       ;;Posicion Player
     .db     #0xFF                       ;;FIN enemigos
     .db     56,48                       ;;Pos enemigo
     .db     48,124                       ;;Pos enemigo
